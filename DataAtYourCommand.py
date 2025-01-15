53f1531b3891de6eacefa6df330b1b0ca2601419
Josh Arbon <jarbon@student.neumont.edu>
2025-01-15 14:31:11 -0700

    most matrix functions working

--- DataAtYourCommand.py
+++ DataAtYourCommand.py
@@ -5,28 +5,43 @@ file_name = "matrix.txt"
 empty_file_matrix = {}
 
 # file stuff
-def add_to_file(matrix: dict, matrix_name: str) -> bool:
+def add_to_file(matrix: dict, matrix_name: str):
     # TODO open file and add new matrix to it
-    pass
+    matrix_objs = get_matrices_from_file()
+    if not matrix_objs:
+        print("OH NO IS BROKEN")
+        return
+    matrix_objs[matrix_name] = matrix
+    try:
+        with open(file_name, "w") as file:
+            json.dump(matrix_objs, file, indent=4)
+    except:
+        print("Couldn't write matrix to file")
+        
+def overwrite_file(matrices: dict):
+    try:
+        file = open(file_name, 'w')
+        file.write(json.dumps(matrices))
+    except:
+        print("Couldn't open file")
+    finally:
+        file.close()
 
 def get_matrices_from_file() -> dict:
-    # TODO open file, load matrices, close file
-    pass
+    try:
+        file = open(file_name, 'r')
+        matrix_objs = json.load(file)
+    except:
+        print("Couldn't open file")
+    finally:
+        file.close()
+    return matrix_objs
 
 def view_whole_file(options: list):
     if len(options) > 0:
         print("'ls' takes no arguments")
         return
-    matrix_data = None
-    file = None
-    try:
-        file = open(file_name, 'r')
-        matrix_data = json.load(file)
-    except:
-        pass
-    finally:
-        file.close()
-        
+    matrix_data = get_matrices_from_file()    
     for matrix_name in matrix_data:
         print_matrix(matrix_data[matrix_name], matrix_name)
 # end file stuff
@@ -34,25 +49,76 @@ def view_whole_file(options: list):
 # matrix functions
 def print_matrix(matrix: dict, matrix_name: str):
     print("")
-    print(f"name    - {matrix_name}")
+    print(f"name    - '{matrix_name}'")
     for matrix_sub in matrix:
             print(f"{matrix_sub} " + " " * (7 - len(matrix_sub)) + f"- {matrix[matrix_sub]}")
     print("")
 
+def parse_matrix(properties: list):
+    try:
+        rows = int(properties[0])
+        string_values = properties[1].split(',')
+        if (len(string_values) % rows != 0):
+            print("Values needs to be multiple of row_count")
+            return
+        float_values = []
+        for values in string_values:
+            float_values.append(float(values))        
+    except:
+        print("row_count and values need to be numbers")
+        return
+    return {
+        "rows"    : rows,
+        "columns" : int(len(float_values)/rows),
+        "values"  : float_values
+    }
+
 def multiply_matrix(options: list):
-    pass
+    if len(options) < 2:
+        print("provide the name of 2 matrices")
+        return
+    add_mat_to_file = False
+    if len(options) == 3:
+        if options[2] != "w":
+            print("use the 'w' option to add matrix to the file after calculation")
+            return
+        add_mat_to_file = True
+    matrix_objs = get_matrices_from_file()
+    mat1 = matrix_objs.get(options[0], None)
+    if not mat1:
+        return
+    mat2 = matrix_objs.get(options[1], None)
+    if not mat2:
+        return 
+    if mat1["columns"] != mat2["rows"]:
+        print("matrices have incompatible dimensions")
+        return
+    first_new_values  = []
+    second_new_values = []
+    for first in range(0, len(mat1["values"])):
+        for second in range(0, mat2["columns"]):
+            if(first % 2) == 0:
+                first_new_values.append(mat1["values"][first] * mat2["values"][second])
+            else:
+                second_new_values.append(mat1["values"][first] * mat2["values"][mat2["columns"] + second])
+    for add_idx in range(0, len(first_new_values)):
+        first_new_values[add_idx] += second_new_values[add_idx]
+    new_mat = {
+        "rows"    : mat1["rows"],
+        "columns" : mat2["columns"],
+        "values"  : first_new_values
+    }
+    if add_mat_to_file:
+        add_to_file(new_mat, f"{options[0]}_*_{options[1]}")
+        print("matrix added to file")
+    print(first_new_values)
 
 def add_matrix(options: list, scale: int=1):
     if len(options) != 2:
         print("provide the name of two matrices")
-    file = None
-    matrix_data = None
-    try:
-        file = open(file_name, 'r')
-        matrix_data = json.load(file)
-        file.close()
-    except:
-        pass
+    scale = 1 if scale > -1 else scale
+    scale = -1 if scale < -1 else scale
+    matrix_data = get_matrices_from_file()
     matrix_name_1 = options[0]
     matrix_name_2 = options[1]
     matrix_obj_1 = matrix_data.get(matrix_name_1, None)
@@ -75,11 +141,9 @@ def add_matrix(options: list, scale: int=1):
     if matrix_1_rows != matrix_2_rows or len(matrix_1_values) != len(matrix_2_values):
         print(f"{matrix_name_1} and {matrix_name_2} have incompatible dimensions")
         return
-    join_string = None
-    if scale == 1:
-        join_string = "_+_"
-    else:
-        join_string = "_-_"
+    join_string = "+"
+    if scale == -1:
+        join_string = "-"
     new_matrix_name = matrix_name_1 + join_string + matrix_name_2
     new_matrix = {
         new_matrix_name : {
@@ -99,14 +163,10 @@ def scale_matrix(options: list):
     if len(options) != 2:
         print("Provide a matrix name and a scalar value")
         return
-    file = None
-    matrix_data = None
-    try:
-        file = open(file_name, 'r')
-        matrix_data = json.load(file)
-        file.close()
-    except:
-        print("couldn't open file")
+
+    matrix_data = get_matrices_from_file()
+    if not matrix_data:
+        return
     matrix_name = options[0]
     matrix_obj = matrix_data.get(matrix_name, None)
     if not matrix_obj:
@@ -123,7 +183,35 @@ def scale_matrix(options: list):
     print_matrix(matrix_obj, matrix_name)
 
 def transpose_matrix(options: list):
-    pass
+    if len(options) < 1:
+        print("Provide the name of the matrix to transpose")
+        return
+    write = False
+    if len(options) == 2:
+        if options[1] == 'w':
+            write = True
+    matrix_objs = get_matrices_from_file()
+    if not matrix_objs:
+        print("waaaaa")
+        return
+    mat_to_transpose = matrix_objs.get(options[0], None)
+    if not mat_to_transpose:
+        print("aaaaaaaaa")
+        return
+    new_list = []
+    for column in range(0, mat_to_transpose["columns"]):
+        for row in range(0, mat_to_transpose["rows"]):
+            new_list.append(mat_to_transpose["values"][column + row*mat_to_transpose["columns"]])
+            pass
+    write_mat = {
+    "rows"    : mat_to_transpose["columns"],
+    "columns" : mat_to_transpose["rows"],
+    "values"  : new_list
+    }
+    trans_mat_name = options[0] + "t"
+    if write:
+        add_to_file(write_mat, trans_mat_name)
+    print_matrix(write_mat, trans_mat_name)
 
 def find_determinate(options: list):
     pass
@@ -169,9 +257,7 @@ ex_commands_help = {
 def execute(options: list):
     while True:
         user_input = input("matrixinator> ").lower()
-        
         user_input = user_input.split(" ")
-        
         if user_input[0] not in ex_commands:
             print("not a matrix operation")
         elif(user_input[0] == "exit" or user_input[0] == "x"):
@@ -186,118 +272,55 @@ def add(options: list):
     if len(options) != 3:
         print("Not enough arguments, provide - name row_count values\nExample, add matrix1 2 1,2,3,4")
         return
-    
-    rows = 1
-    float_values = []
-    try:
-        rows = int(options[1])
-        string_values = options[2].split(',')
-        if (len(string_values) % rows != 0):
-            print("Values needs to be multiple of row_count")
-            return
-        for values in string_values:
-            float_values.append(float(values))        
-    except:
-        print("row_count and values need to be numbers")
-    
-    file = None
-    matrix_data = None
-    try:
-        file = open(file_name, 'r')
-        matrix_data = json.load(file)
-        matrix_data[options[0]] = {
-            "rows" : rows,
-            "columns" : int(len(float_values)/rows),
-            "values" : float_values
-        }
-        file.close()
-    except:
-        print(f"Couldn't open file: {file_name}")
-    
-    global empty_file_matrix
-    try:
-        with open(file_name, 'w') as file:
-            json.dump(matrix_data, file, indent=4)
-    except json.JSONDecodeError:
-        print("Couldn't dump matrix into file. Dumping default in.")
-        file.close()
-        with open(file_name, 'w') as file:
-            file.write(json.dumps(empty_file_matrix)) 
-        file.close()       
+    matrix_parsed = parse_matrix(options[1:])
+    if not matrix_parsed:
+        return
+    matrix_add = matrix_parsed
+    add_to_file(matrix_add, options[0])
 
 def remove(options: list):
     if len(options) != 1:
         print("Provide one name of matrix to remove")
         return
-    try:
-        file = open(file_name, 'r+')
-        matrix_data = json.load(file)
-        if matrix_data.pop(options[0], None):
-            print(f"Matrix {options[0]} was removed")
-        else:
-            print("No matrix exists with that name")
-        file.close()
-        file = open(file_name, 'w')
-        file.write(json.dumps(matrix_data))
-        file.close()
-    except:
-        print("something went wrong")
+    matrix_data = get_matrices_from_file()
+    if not matrix_data:
+        return
+    if matrix_data.pop(options[0], None):
+        print(f"Matrix {options[0]} was removed")
+    else:
+        print("No matrix exists with that name")
+        return
+    overwrite_file(matrix_data)
 
 def search(options: list):
     if len(options) != 1:
         print("Provide the name of the matrix you are searching for")
         return
-    try:
-        file = open(file_name, 'r')
-        matrix_data = json.load(file)
-        if matrix_data.get(options[0], None):
-            print(f"\n{options[0]}")
-            for matrix_sub in matrix_data[options[0]]:
-                print(f"{matrix_sub} - {matrix_data[options[0]][matrix_sub]}")
-        else:
-            print("matrix not found in file")
-    except:
-        print("Couldn't open file")
+    matrix_data = get_matrices_from_file()
+    if not matrix_data:
+        return
+    matrix_name = options[0]
+    if matrix_data.get(matrix_name, None):
+        print_matrix(matrix_data[matrix_name], matrix_name)
+    else:
+        print("matrix not found in file")
 
 def update(options: list):
     if len(options) != 3:
         print("provide all necessary parameters")
         return
-    file = None
-    matrix_data = None
-    try:
-        file = open(file_name, 'r')
-        matrix_data = json.load(file)
-        if not matrix_data.get(options[0], None):
-            print("no matrix with that name")
-            return        
-        try:
-            rows = 1
-            float_values = []
-            rows = int(options[1])
-            string_values = options[2].split(',')
-            if (len(string_values) % rows != 0):
-                print("Values needs to be multiple of row_count")
-                return
-            for values in string_values:
-                float_values.append(float(values)) 
-            matrix_data[options[0]] = {
-                "rows" : rows,
-                "values" : float_values
-            }
-        except:
-            print("numbers required")
-        file.close()
-    except:
-        print("couldnt open file1")
-    finally:
-        file.close()
-    try:
-        with open(file_name, 'w') as file:
-            json.dump(matrix_data, file, indent=4)
-    except:
-        file.close()
-        print("couldn't open file2")
+    matrix_name = options[0]
+    matrix_data = get_matrices_from_file()
+    if not matrix_data:
+        return
+    if not matrix_data.get(matrix_name, None):
+        print("no matrix with that name")
+        return
+    matrix_parsed = parse_matrix(options[1:])
+    if not matrix_parsed:
+        return
+    matrix_data[matrix_name] = matrix_parsed
+    overwrite_file(matrix_data)
 
 def help(options: list):
     print("-" * 55)
